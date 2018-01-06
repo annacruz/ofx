a7128a3aac05c49c45a53d409d5770e0553e8540
Jais Cheema <jaischeema@gmail.com>
2018-01-06 19:51:10 +1100

    Add occurred_at attribute for transaction mapped to DTUSER value

--- lib/ofx/parser/ofx102.rb
+++ lib/ofx/parser/ofx102.rb
@@ -81,6 +81,8 @@ module OFX
       end
 
       def build_transaction(element)
+        occurred_at = build_date(element.search("dtuser").inner_text) rescue nil
+
         OFX::Transaction.new({
           :amount            => build_amount(element),
           :amount_in_pennies => (build_amount(element) * 100).to_i,
@@ -91,6 +93,7 @@ module OFX
           :check_number      => element.search("checknum").inner_text,
           :ref_number        => element.search("refnum").inner_text,
           :posted_at         => build_date(element.search("dtposted").inner_text),
+          :occurred_at       => occurred_at,
           :type              => build_type(element),
           :sic               => element.search("sic").inner_text
         })
--- spec/ofx/ofx102_spec.rb
+++ spec/ofx/ofx102_spec.rb
@@ -5,11 +5,11 @@ describe OFX::Parser::OFX102 do
     @ofx = OFX::Parser::Base.new("spec/fixtures/sample.ofx")
     @parser = @ofx.parser
   end
-  
+
   it "should have a version" do
     OFX::Parser::OFX102::VERSION.should == "1.0.2"
   end
-  
+
   it "should set headers" do
     @parser.headers.should == @ofx.headers
   end
@@ -22,7 +22,7 @@ describe OFX::Parser::OFX102 do
   it "should set body" do
     @parser.body.should == @ofx.body
   end
-  
+
   it "should set account" do
     @parser.account.should be_a_kind_of(OFX::Account)
   end
@@ -30,17 +30,17 @@ describe OFX::Parser::OFX102 do
   it "should set account" do
     @parser.sign_on.should be_a_kind_of(OFX::SignOn)
   end
-  
+
   it "should know about all transaction types" do
     valid_types = [
-      'CREDIT', 'DEBIT', 'INT', 'DIV', 'FEE', 'SRVCHG', 'DEP', 'ATM', 'POS', 'XFER', 
+      'CREDIT', 'DEBIT', 'INT', 'DIV', 'FEE', 'SRVCHG', 'DEP', 'ATM', 'POS', 'XFER',
       'CHECK', 'PAYMENT', 'CASH', 'DIRECTDEP', 'DIRECTDEBIT', 'REPEATPMT', 'OTHER'
     ]
     valid_types.sort.should == OFX::Parser::OFX102::TRANSACTION_TYPES.keys.sort
-    
+
     valid_types.each do |transaction_type|
       transaction_type.downcase.to_sym.should equal OFX::Parser::OFX102::TRANSACTION_TYPES[transaction_type]
     end
   end
-  
+
 end
