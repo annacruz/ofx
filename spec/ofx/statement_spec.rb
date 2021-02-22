29621268967adac25c95365a32b733f9713e81e3
Nicolas <nyko-23@hotmail.com>
2021-02-22 14:19:52 -0300

    Set time zone on some date tests.

--- spec/ofx/statement_spec.rb
+++ spec/ofx/statement_spec.rb
@@ -12,11 +12,11 @@ describe OFX::Statement do
     end
 
     it "returns start date" do
-      statement.start_date.should == Time.parse("2009-10-09 08:00:00")
+      statement.start_date.should == Time.parse("2009-10-09 08:00:00 +0000")
     end
 
     it "returns end date" do
-      statement.end_date.should == Time.parse("2009-11-03 08:00:00")
+      statement.end_date.should == Time.parse("2009-11-03 08:00:00 +0000")
     end
 
     it "returns account" do
@@ -42,7 +42,7 @@ describe OFX::Statement do
       end
 
       it "returns balance date" do
-        balance.posted_at.should == Time.parse("2009-11-01")
+        balance.posted_at.should == Time.parse("2009-11-01 00:00:00 +0000")
       end
     end
 
@@ -58,7 +58,7 @@ describe OFX::Statement do
       end
 
       it "returns available balance date" do
-        available_balance.posted_at.should == Time.parse("2009-11-01")
+        available_balance.posted_at.should == Time.parse("2009-11-01 00:00:00 +0000")
       end
 
       context "when AVAILBAL not found" do
@@ -79,11 +79,11 @@ describe OFX::Statement do
     end
 
     it "returns start date" do
-      statement.start_date.should == Time.parse("2007-05-09 12:00:00")
+      statement.start_date.should == Time.parse("2007-05-09 12:00:00 +0000")
     end
 
     it "returns end date" do
-      statement.end_date.should == Time.parse("2007-06-08 12:00:00")
+      statement.end_date.should == Time.parse("2007-06-08 12:00:00 +0000")
     end
 
     it "returns account" do
@@ -108,7 +108,7 @@ describe OFX::Statement do
       end
 
       it "returns balance date" do
-        balance.posted_at.should == Time.parse("2007-06-23 19:20:13")
+        balance.posted_at.should == Time.parse("2007-06-23 19:20:13 +0000")
       end
     end
 
@@ -124,7 +124,7 @@ describe OFX::Statement do
       end
 
       it "returns available balance date" do
-        available_balance.posted_at.should == Time.parse("2007-06-23 19:20:13")
+        available_balance.posted_at.should == Time.parse("2007-06-23 19:20:13 +0000")
       end
     end
   end
